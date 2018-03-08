c397fd078dbdd410b30b2b90daf572f75d8d8592
liuduanyang <1069831692@qq.com>
2018-03-08 14:37:25 +0800

    改进小数计算出错bug

--- 01-first-app/first-app.js
+++ 01-first-app/first-app.js
@@ -5,9 +5,21 @@ var mj=document.getElementById("mj");
 var tip1=document.getElementById("tip1");
 var tip2=document.getElementById("tip2");
 
+window.onload=function(){
+    myHight.focus();
+}
+
+// myHight.onblur=function(){
+//     console.log(1);
+//     if(!myHight.value){
+//         myHight.focus();
+//     }
+// }
+
 function a(){
     if(!myHight.value){
         tip1.style.display="block";
+        myHight.focus();
     }else{
         tip1.style.display="none";
     }
@@ -20,6 +32,7 @@ myHight.onblur=a;
 function b(){
     if(!myWidth.value){
         tip2.style.display="block";
+        myWidth.focus();
     }else{
         tip2.style.display="none";
     }
@@ -31,7 +44,30 @@ myWidth.onblur=b;
 
 var js=document.querySelector(".js");
 
+function FloatZc(arg1,arg2){  
+    var r1,r2,m; 
+    try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}; //参数1为整数}; 
+    try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0;} //参数2为整数};
+    m=Math.pow(10,Math.max(r1,r2));  //取其中较大的位数
+    return (arg1*m*2+arg2*m*2)/m;   //先将arg1和arg2转换为整数进行计算，然后再转换回浮点数
+} 
+
+function FloatMj(arg1,arg2){  
+    var r1,r2,m; 
+    try{r1=arg1.toString().split(".")[1].length}catch(e){r1=0}; //参数1为整数}; 
+    try{r2=arg2.toString().split(".")[1].length}catch(e){r2=0;} //参数2为整数};
+    m=Math.pow(10,Math.max(r1,r2));  //取其中较大的位数
+    return (arg1*m*arg2*m)/(m*m);   //先将arg1和arg2转换为整数进行计算，然后再转换回浮点数
+} 
+
 js.onclick=function(){
+    var result1 = (myWidth.value.toString()).indexOf(".");
+    var result2 = (myHight.value.toString()).indexOf(".");
+    if(result1 != -1 || result2 != -1) {
+        zc.value=FloatZc(myWidth.value,myHight.value);
+        mj.value=FloatMj(myWidth.value,myHight.value);
+        return;
+    } 
     zc.value=myWidth.value*2+myHight.value*2;
     mj.value=myWidth.value*myHight.value;
 }
